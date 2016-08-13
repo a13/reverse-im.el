eaf8544cf643b0ee61d8d3c3c65abd8bd34d6ab0
DK <beerandhot@gmail.com>
2016-08-13 12:08:41 +0300

    fix example

--- reverse-im.el
+++ reverse-im.el
@@ -7,7 +7,7 @@
 ;;; Commentary:
 ;; Creates translation mappings for input-methods other than default english one,
 ;; so all (?) keybindings are usable while non-default system layout is active.
-;; Example usage: (activate-reverse-im "russian-computer")
+;; Example usage: (reverse-im-activate "russian-computer")
 
 ;; Main idea and code taken from http://ru-emacs.livejournal.com/82428.html
 
