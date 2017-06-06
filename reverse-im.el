df6180491900ec68916fa59156a9bc2b47a25c6c
DK <beerandhot@gmail.com>
2017-06-06 20:53:36 +0300

    use function-key-map

--- reverse-im.el
+++ reverse-im.el
@@ -16,10 +16,6 @@
 (require 'cl-extra)
 (require 'cl-macs)
 
-(defvar reverse-im-im-list
-  nil
-  "List of input methods to activate on new frame creation.")
-
 (defun reverse-im--key-def (map mod)
   "Return a list of last two arguments for `define-key' for MAP with MOD modifier."
   (cl-destructuring-bind (keychar def) map
@@ -46,23 +42,15 @@
   "Activate KD key/definition list."
   (when kd
     (cl-destructuring-bind (key def) kd
-      (define-key local-function-key-map key def))))
+      (define-key function-key-map key def))))
 
 (defun reverse-im-activate-im (input-method)
   "Activate INPUT-METHOD."
   (mapc #'reverse-im--activate-key-def (reverse-im--translation-table input-method)))
 
-(defun reverse-im-frame-hook (f)
-  "Create reversed input method for F frame."
-  (let ((frame f))
-    (run-at-time nil nil
-                 #'(lambda ()
-                     (unless (and (daemonp) (eq frame terminal-frame))
-                       (mapc #'reverse-im-activate-im reverse-im-im-list))))))
-
 (defun reverse-im-read-passwd-override-keymap (orig-fun &rest args)
   "Override `read-passwd' keymap."
-  (let ((local-function-key-map nil)
+  (let ((function-key-map nil)
         (read-passwd-map (let ((map read-passwd-map))
                            (set-keymap-parent map minibuffer-local-map)
                            (define-key map [return] #'exit-minibuffer)
@@ -72,22 +60,13 @@
 
 (defun reverse-im-activate (input-method)
   "Activates the reverse mapping for INPUT-METHOD.
-If daemon mode is active, adds hook to `after-make-frame-functions'
 Example usage: (reverse-im-activate \"russian-computer\")"
-  (when (daemonp)
-    (cl-pushnew input-method reverse-im-im-list :test #'equal)
-    (add-hook 'after-make-frame-functions #'reverse-im-frame-hook))
   (advice-add 'read-passwd :around #'reverse-im-read-passwd-override-keymap)
   (reverse-im-activate-im input-method))
 
 (defun reverse-im-deactivate (&optional input-method)
   "Deactivate(partially) INPUT-METHOD.
-Remove INPUT-METHOD from `reverse-im-im-list',
-deactivate hook `reverse-im-frame-hook'
 remove advice `reverse-im-read-passwd-override-keymap'."
-  (when input-method
-    (remove input-method reverse-im-im-list))
-  (remove-hook 'after-make-frame-functions #'reverse-im-frame-hook)
   (advice-remove 'read-passwd  #'reverse-im-read-passwd-override-keymap))
 
 (provide 'reverse-im)
