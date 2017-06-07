11453365dcd253a84444898fcc990098bd0e1c64
DK <beerandhot@gmail.com>
2017-06-07 12:07:12 +0300

    store translated keymaps

--- reverse-im.el
+++ reverse-im.el
@@ -16,16 +16,16 @@
 (require 'cl-extra)
 (require 'cl-macs)
 
-(defun reverse-im--key-def (map mod)
-  "Return a list of last two arguments for `define-key' for MAP with MOD modifier."
-  (cl-destructuring-bind (keychar def) map
-    (let ((from (quail-get-translation def (char-to-string keychar) 1)))
-      (and (characterp from) (characterp keychar) (not (= from keychar))
-           (list
-            (vector (append mod (list from)))
-            (vector (append mod (list keychar))))))))
+(defvar reverse-im--keymaps-alist
+  `((t . ,local-function-key-map))
+  "Alist of pairs input-method/translation keymap.")
 
-;; TODO: memoize
+(defun reverse-im--activate-key-def (keymap kd)
+  "Add to KEYMAP KD key/definition list."
+  (when kd
+    (cl-destructuring-bind (key def) kd
+      (define-key keymap key def))))
+
 (defun reverse-im--translation-table (input-method)
   "Generate a translation table for INPUT-METHOD."
   (with-temp-buffer
@@ -38,15 +38,30 @@
           (list nil '(control) '(meta) '(control meta))))
        (cdr (quail-map))))))
 
-(defun reverse-im--activate-key-def (kd)
-  "Activate KD key/definition list."
-  (when kd
-    (cl-destructuring-bind (key def) kd
-      (define-key function-key-map key def))))
+(when nil
+  (reverse-im--im-to-keymap "russian-unipunct")
+  (reverse-im-activate "russian-unipunct")
+  (reverse-im-deactivate)
+  reverse-im--keymaps-alist)
 
-(defun reverse-im-activate-im (input-method)
-  "Activate INPUT-METHOD."
-  (mapc #'reverse-im--activate-key-def (reverse-im--translation-table input-method)))
+(defun reverse-im--im-to-keymap (input-method)
+  "Translation keymap for INPUT-METHOD."
+  (let ((keymap (alist-get input-method reverse-im--keymaps-alist nil)))
+    (or keymap
+        (let ((new-keymap (make-sparse-keymap)))
+          (mapc (apply-partially #'reverse-im--activate-key-def new-keymap)
+                (reverse-im--translation-table input-method))
+          (add-to-list 'reverse-im--keymaps-alist `(,input-method . ,new-keymap))
+          new-keymap))))
+
+(defun reverse-im--key-def (map mod)
+  "Return a list of last two arguments for `define-key' for MAP with MOD modifier."
+  (cl-destructuring-bind (keychar def) map
+    (let ((from (quail-get-translation def (char-to-string keychar) 1)))
+      (and (characterp from) (characterp keychar) (not (= from keychar))
+           (list
+            (vector (append mod (list from)))
+            (vector (append mod (list keychar))))))))
 
 (defun reverse-im-read-passwd-override-keymap (orig-fun &rest args)
   "Override `read-passwd' keymap."
@@ -59,14 +74,19 @@
     (apply orig-fun args)))
 
 (defun reverse-im-activate (input-method)
-  "Activates the reverse mapping for INPUT-METHOD.
+  "Activate the reverse mapping for INPUT-METHOD.
 Example usage: (reverse-im-activate \"russian-computer\")"
-  (advice-add 'read-passwd :around #'reverse-im-read-passwd-override-keymap)
-  (reverse-im-activate-im input-method))
+  (setq local-function-key-map
+        (make-composed-keymap
+         (list
+          (reverse-im--im-to-keymap input-method)
+          local-function-key-map)))
+  (advice-add 'read-passwd :around #'reverse-im-read-passwd-override-keymap))
 
-(defun reverse-im-deactivate (&optional input-method)
-  "Deactivate(partially) INPUT-METHOD.
+(defun reverse-im-deactivate ()
+  "Deactivate translated keymap.
 remove advice `reverse-im-read-passwd-override-keymap'."
+  (setq local-function-key-map (alist-get t reverse-im--keymaps-alist nil))
   (advice-remove 'read-passwd  #'reverse-im-read-passwd-override-keymap))
 
 (provide 'reverse-im)
