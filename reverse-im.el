93d66e20548e2879320b8cb7c56e0fbeaebe06b6
DK <beerandhot@gmail.com>
2017-06-07 14:46:43 +0300

    update readme

--- reverse-im.el
+++ reverse-im.el
@@ -4,8 +4,8 @@
 ;; Homepage: https://github.com/a13/reverse-im.el
 
 ;;; Commentary:
-;; Creates translation mappings for input-methods other than default english one,
-;; so all (?) keybindings are usable while non-default system layout is active.
+;; Overrides `function-key-map' for preferred input-method to translate input sequences
+;; to english, so we can use Emacs bindings while non-default system layout is active.
 ;; Example usage: (reverse-im-activate "russian-computer")
 
 ;; Main idea and some code taken from http://ru-emacs.livejournal.com/82428.html
