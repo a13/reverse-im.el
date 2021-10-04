d794d2deafbeb2b6e6bd540d7d8f659f5956db85
DK <beerandhot@gmail.com>
2021-10-04 22:28:54 +0300

    add more tests

--- reverse-im-test.el
+++ reverse-im-test.el
@@ -1,11 +1,49 @@
 (require 'reverse-im)
-(require 'cl-lib)
+(require 'seq)
+
+(defun seq-set-equal-internal-p (s1 s2)
+  (seq-set-equal-p s1 s2 'equal))
+
+(defun seq-set-equal-deep-p (ss1 ss2)
+  (seq-set-equal-p
+   ss1 ss2
+   (lambda (s1 s2)
+     (seq-set-equal-p s1 s2 'equal))))
 
 (ert-deftest reverse-im--modifiers-combos-test ()
   (should
-   (null
-    (cl-set-difference
-     (reverse-im--modifiers-combos '(control meta super))
-     '(nil (super) (meta) (meta super) (control)
-           (control super) (control meta) (control meta super))
-     :test 'equal))))
+   (seq-set-equal-deep-p
+    (reverse-im--modifiers-combos '(meta control))
+    '(nil (meta) (control) (control meta))))
+  (should
+   (seq-set-equal-deep-p
+    (reverse-im--modifiers-combos '(control meta))
+    '(nil (meta) (control) (control meta))))
+  ;; FIXME:
+  ;; (should
+  ;;  (null
+  ;;   (reverse-im--modifiers-combos '())
+  ;;   '())) ;or '(nil?)
+  ;; (should
+  ;;  (null
+  ;;   (reverse-im--modifiers-combos '(control control))
+  ;;   '(nil (control))))
+  (should
+   (equal
+    (reverse-im--modifiers-combos '(control))
+    '(nil (control)))))
+
+(ert-deftest reverse-im--key-def-internal-test ()
+  (let ((reverse-im-modifiers '(control)))
+    (seq-set-equal-deep-p
+     (reverse-im--key-def-internal 113 1093)
+     '(([(1093)] [(113)]) ([(control 1093)] [(control 113)]))))
+  ;; already in default layout
+  (let ((reverse-im-modifiers '(control))
+        (quail-keyboard-layout '(1093)))
+    (null
+     (reverse-im--key-def-internal 113 1093)))
+  ;; the same
+  (let ((reverse-im-modifiers '(control)))
+    (null
+     (reverse-im--key-def-internal 1093 1093))))
